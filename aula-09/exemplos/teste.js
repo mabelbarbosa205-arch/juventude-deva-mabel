e404f5cbeefd6ba9f55a2a90ea5980d56904dd90
mabelbarbosa205-arch <mabelbarbosa205@gmail.com>
2026-09-22 19:14:16 +0000

    aula09

---
+++ aula-09/exemplos/teste.js
@@ -0,0 +1,21 @@
+function verificarIdade(idade) { 
+  if (idade >= 18) { 
+    console.log("Maior de idade");
+  } 
+ else if(idade===30){
+    console.log("mais")
+ }
+  else { 
+    console.log("Menor de idade"); 
+  } 
+} 
+
+verificarIdade(20);
+
+function verificarIdade(idade) {
+if (idade >= 18) {
+    console.log(idade===30) {
+
+    }
+}
+}
